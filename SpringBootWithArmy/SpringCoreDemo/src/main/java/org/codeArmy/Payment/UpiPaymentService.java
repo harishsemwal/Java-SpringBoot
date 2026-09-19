254ad6542206049ccfaaa92e728d9172867c9359
harishsemwal <harishsemwal581@gmail.com>
2026-09-19 14:10:24 +0530

    SP-0005-BeanLifeCycle: Bean LifeCycle.

--- SpringBootWithArmy/SpringCoreDemo/src/main/java/org/codeArmy/Payment/UpiPaymentService.java
+++ SpringBootWithArmy/SpringCoreDemo/src/main/java/org/codeArmy/Payment/UpiPaymentService.java
@@ -1,15 +1,15 @@
-package org.codeArmy.Payment;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-//@Component
-//@Qualifier("upi")
-public class UpiPaymentService implements PaymentService{
-    @Override
-    public void pay() {
-        System.out.println("UPI Payment Service Done...");
-    }
-}
-
-//  Qualifier Example - tomorrow....
+//package org.codeArmy.Payment;
+//
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.stereotype.Component;
+//
+////@Component
+////@Qualifier("upi")
+//public class UpiPaymentService implements PaymentService{
+//    @Override
+//    public void pay() {
+//        System.out.println("UPI Payment Service Done...");
+//    }
+//}
+//
+////  Qualifier Example - tomorrow....
